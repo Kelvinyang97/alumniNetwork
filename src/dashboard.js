bdaf9fe9e49dc9052b18c5be9f6bccebd45a1731
Kelvin Yang <z5125244@ad.unsw.edu.au>
2019-10-19 16:56:42 +1100

    fix conflicts

--- src/dashboard.js
+++ src/dashboard.js
@@ -15,7 +15,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
 import { createMuiTheme } from '@material-ui/core/styles';
-import Image from './graduation-feature_1-2.jpg'
+// import Image from './graduation-feature_1-2.jpg'
 
 const colors = createMuiTheme({
     palette: {
